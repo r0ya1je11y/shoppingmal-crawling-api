4bf0e27c4501a4de1b0a1a3f8f52c4241a4bc5b4
r0ya1je11y <skydnk4332@gmail.com>
2020-05-25 00:01:08 +0900

    Add automatic login structure
    Delete debug code
    Remove unnessary module

--- crawling.py
+++ crawling.py
@@ -1,41 +1,65 @@
 import ssl, codecs, time, urllib3, requests, json
-from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver import ActionChains
+from selenium.common.exceptions import ElementNotVisibleException
 from collections import OrderedDict
 from parse import *
 
 context = ssl._create_unverified_context()
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+# webdriver path settings
 path = "C:/Users/skydn/downloads/chromedriver.exe"
 driver = webdriver.Chrome(path)
 
+# Make a url of a product
 baseurl = "https://item.taobao.com/item.htm?id="
 productID = input()
 url = baseurl + productID
 driver.get(url)
 
 # Click the alert window
-#if driver.find_element_by_id('simpleAlert'):
-#    inputElement = driver.find_element_by_id('simpleAlert')
-#    inputElement.click()
-#    time.sleep(2)
+try:
+    IsAlart = driver.switch_to_alert()
+    IsAlart.accept()
+except:
+    "There is no alert"
+
+# Login Taobao
+driver.implicitly_wait(5)
+
+driver.switch_to_frame(driver.find_element_by_id('sufei-dialog-content'))
+
+driver.find_element_by_id('fm-login-id').send_keys('<ID>') # Input the ID
+driver.find_element_by_id('fm-login-password').send_keys('<Password>') # Input the Password
+
+time.sleep(3)
+
+source = driver.find_element_by_id('nc_1_n1z')
+destination = driver.find_element_by_id('nc_1__scale_text')
+action = ActionChains(driver)
+action.click_and_hold(source).move_to_element_with_offset(destination, 250, 20).pause(1).release().perform() # Drag-and-drop slidebar
+
+time.sleep(1)
+
+driver.find_element_by_xpath('//*[@id="login-form"]/div[4]/button').click() # Click the login button
+
+driver.switch_to_default_content()
+
+time.sleep(2)
 
 # Take a 'BeautifulSoup'
-result = urlopen(url, context=context)
-time.sleep(10)
-bsObj = BeautifulSoup(result.read(), "html.parser")
+html = driver.page_source
+bsObj = BeautifulSoup(html, "html.parser")
 
 # Parse a name of the product
 Name = bsObj.find('h3', {'class':'tb-main-title'})
 productName = Name.text.strip()
-print(productName)
 
-print("============")
 # Parse a option of the product
 Skin = bsObj.find('div', {'class':'tb-skin'})
 Option = Skin.find('dl').find('dd').find_all('a')
@@ -43,9 +67,6 @@ Option = Skin.find('dl').find('dd').find_all('a')
 optionTitle = []
 for title in Option:
     optionTitle.append(title.find('span').text)
-    print(title.find('span').text) # Option Title
-
-print("============")
 
 optionImage = []
 for image in Option:
@@ -53,23 +74,18 @@ for image in Option:
         styleTag = image.get('style')
         result = parse("background:url({}) center no-repeat;", styleTag)
         optionImage.append(result[0])
-        print(result[0]) # Option Image if exist
     else:
         optionImage.append("")
 
-print("============")
-
 productOption = {}
 for i in range(0, len(optionTitle)-1):
-    productOption[optionTitle[i]] = optionImage[i]
+    productOption[optionTitle[i]] = optionImage[i] # Make dictionary of {optionTitle:optionImage}
+
 # Parse a price of the product
 Price_div = bsObj.find('ul', class_="tb-meta")
 print(Price_div)
 
-#driver.close()
-
-
-#========================================
+driver.close()
 
 # Make .json file
 file_data = OrderedDict()
@@ -78,8 +94,6 @@ file_data["name"] = productName
 file_data["id"] = productID
 #file_data["price"] = productPrice
 file_data["option"] = productOption
-
-print(json.dumps(file_data, ensure_ascii=False, indent='\t'))
 
 with open('data/taobao.json', 'w', encoding="utf-8") as make_file:
     json.dump(file_data, make_file, ensure_ascii=False, indent='\t')
