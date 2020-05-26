40d996470cc38f0f6f347e95b1984932f1828345
r0ya1je11y <skydnk4332@gmail.com>
2020-05-26 12:48:29 +0900

    Make crawling.py module
    Modularization crawling.py
    By using the "crawlingTaobao" method in crawling.py, taobao can be crawled

--- crawling.py
+++ crawling.py
@@ -2,6 +2,7 @@ import ssl, codecs, time, urllib3, requests, json
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import ActionChains
@@ -9,91 +10,102 @@ from selenium.common.exceptions import ElementNotVisibleException
 from collections import OrderedDict
 from parse import *
 
-context = ssl._create_unverified_context()
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+def CrawlingTaobao(productID, userID, userPasswd):
+    context = ssl._create_unverified_context()
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-# webdriver path settings
-path = "C:/Users/skydn/downloads/chromedriver.exe"
-driver = webdriver.Chrome(path)
+    # webdriver path settings
+    path = "C:/Users/skydn/downloads/chromedriver.exe"
+    driver = webdriver.Chrome(path)
 
-# Make a url of a product
-baseurl = "https://item.taobao.com/item.htm?id="
-productID = input()
-url = baseurl + productID
-driver.get(url)
+    # Make a url of a product
+    baseurl = "https://item.taobao.com/item.htm?id="
+    url = baseurl + productID
+    driver.get(url)
 
-# Click the alert window
-try:
-    IsAlart = driver.switch_to_alert()
-    IsAlart.accept()
-except:
-    "There is no alert"
+    # Click the alert window
+    try:
+        IsAlart = driver.switch_to_alert()
+        IsAlart.accept()
+    except:
+        "There is no alert"
 
-# Login Taobao
-driver.implicitly_wait(5)
+    # Login Taobao
+    driver.implicitly_wait(5)
 
-driver.switch_to_frame(driver.find_element_by_id('sufei-dialog-content'))
+    driver.switch_to_frame(driver.find_element_by_id('sufei-dialog-content'))
 
-driver.find_element_by_id('fm-login-id').send_keys('<ID>') # Input the ID
-driver.find_element_by_id('fm-login-password').send_keys('<Password>') # Input the Password
+    driver.find_element_by_id('fm-login-id').send_keys(userID) # Input the ID
+    driver.find_element_by_id('fm-login-password').send_keys(userPasswd) # Input the Password
 
-time.sleep(3)
+    time.sleep(3)
 
-source = driver.find_element_by_id('nc_1_n1z')
-destination = driver.find_element_by_id('nc_1__scale_text')
-action = ActionChains(driver)
-action.click_and_hold(source).move_to_element_with_offset(destination, 250, 20).pause(1).release().perform() # Drag-and-drop slidebar
+    source = driver.find_element_by_id('nc_1_n1z')
+    destination = driver.find_element_by_id('nc_1__scale_text')
+    action = ActionChains(driver)
+    action.click_and_hold(source).move_to_element_with_offset(destination, 250, 20).pause(1).release().perform() # Drag-and-drop slidebar
 
-time.sleep(1)
+    time.sleep(1)
 
-driver.find_element_by_xpath('//*[@id="login-form"]/div[4]/button').click() # Click the login button
+    driver.find_element_by_xpath('//*[@id="login-form"]/div[4]/button').click() # Click the login button
 
-driver.switch_to_default_content()
+    driver.switch_to_default_content()
 
-time.sleep(2)
+    time.sleep(3)
 
-# Take a 'BeautifulSoup'
-html = driver.page_source
-bsObj = BeautifulSoup(html, "html.parser")
+    # Take a 'BeautifulSoup'
+    html = driver.page_source
+    bsObj = BeautifulSoup(html, "html.parser")
 
-# Parse a name of the product
-Name = bsObj.find('h3', {'class':'tb-main-title'})
-productName = Name.text.strip()
+    # Parse a name of the product
+    Name = bsObj.find('h3', {'class':'tb-main-title'})
+    productName = Name.text.strip()
 
-# Parse a option of the product
-Skin = bsObj.find('div', {'class':'tb-skin'})
-Option = Skin.find('dl').find('dd').find_all('a')
+    # Parse a option of the product
+    Skin = bsObj.find('div', {'class':'tb-skin'})
+    Option = Skin.find('dl').find('dd').find_all('a')
 
-optionTitle = []
-for title in Option:
-    optionTitle.append(title.find('span').text)
+    optionTitle = []
+    for title in Option:
+        optionTitle.append(title.find('span').text)
 
-optionImage = []
-for image in Option:
-    if image.get('style') is not None:
-        styleTag = image.get('style')
-        result = parse("background:url({}) center no-repeat;", styleTag)
-        optionImage.append(result[0])
-    else:
-        optionImage.append("")
+    optionImage = []
+    for image in Option:
+        if image.get('style') is not None:
+            styleTag = image.get('style')
+            result = parse("background:url({}) center no-repeat;", styleTag)
+            optionImage.append(result[0])
+        else:
+            optionImage.append("")
 
-productOption = {}
-for i in range(0, len(optionTitle)-1):
-    productOption[optionTitle[i]] = optionImage[i] # Make dictionary of {optionTitle:optionImage}
+    # Parse a Price of the product
+    driverOption = driver.find_elements_by_css_selector('#J_isku > div > dl.J_Prop.tb-prop.tb-clear.J_Prop_Color > dd > ul > li > a')
+    productPrice = []
+    for option in driverOption:
+        option.send_keys(Keys.ENTER)
+        try:
+            driver.find_element((By.ID, 'J_PromoPriceNum'))
+            productPrice.append(driver.find_element_by_id('J_PromoPriceNum').text)
+        except:
+            productPrice.append(driver.find_element_by_class_name('tb-rmb-num').text)
 
-# Parse a price of the product
-Price_div = bsObj.find('ul', class_="tb-meta")
-print(Price_div)
+    # Make dictionary for all information of product
+    productOption = {}
+    for i in range(0, len(optionTitle)-1):
+        productInfo = {}
+        productInfo['price'] = productPrice[i]
+        productInfo['image'] = optionImage[i]
+        productOption[optionTitle[i]] = productInfo # Make dictionary of {optionTitle:optionImage}
 
-driver.close()
+    driver.close()
 
-# Make .json file
-file_data = OrderedDict()
+    # Make .json file
+    file_data = OrderedDict()
 
-file_data["name"] = productName
-file_data["id"] = productID
-#file_data["price"] = productPrice
-file_data["option"] = productOption
+    file_data["name"] = productName
+    file_data["id"] = productID
+    file_data["url"] = url
+    file_data["option"] = productOption
 
-with open('data/taobao.json', 'w', encoding="utf-8") as make_file:
-    json.dump(file_data, make_file, ensure_ascii=False, indent='\t')
+    with open('data/' + productID + '.json', 'w', encoding="utf-8") as make_file:
+        json.dump(file_data, make_file, ensure_ascii=False, indent='\t')
